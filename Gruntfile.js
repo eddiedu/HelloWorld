6139bf79d719270a5f35bfe8ccda4bf56fdc7505
Eduardo Fagundes <eddiedu@gmail.com>
2020-01-23 16:54:49 -0300

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,50 @@
+/* global process:true */
+"use strict";
 module.exports = function (grunt) {
-	"use strict";
+	// Variables from environment
+
+	// Project properties
+	var webAppDir = "webapp";
+	var targetDir = "dist";
+
+	// Project configuration.
+	// grunt.initConfig({
+	var config = {
+		eslint: {
+			options: {
+				configFile: ".eslintrc.js"
+			},
+			target: [webAppDir + "/**/*.js"]
+		}
+	}; //);
+
+	grunt.loadNpmTasks("grunt-eslint");
 	grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-build");
-	grunt.registerTask("default", [
-		"clean",
-		"lint",
-		"build"
-	]);
-	grunt.loadNpmTasks("@sap/grunt-sapui5-bestpractice-test");
-	grunt.registerTask("unit_and_integration_tests", ["test"]);
-	grunt.config.merge({
-		coverage_threshold: {
-			statements: 0,
-			branches: 100,
-			functions: 0,
-			lines: 0
+
+	grunt.loadNpmTasks('grunt-openui5');
+
+	grunt.config.merge(config);
+
+	grunt.registerTask("check-lint", "Check validation", function () {
+		var validation = grunt.file.readJSON(targetDir + "/di.code-validation.core_issues.json"),
+			hasErrors = false;
+		for (var check in validation.results) {
+			grunt.log.writeln("Result for: " + check);
+			for (var file in validation.results[check].issues) {
+				validation.results[check].issues[file].forEach(function (error) {
+					if (error.severity === "error") {
+						grunt.log.error(error.path + "(" + error.line + "," + error.column + ") : " + error.message).error();
+						hasErrors = true;
+					}
+				});
+			}
+		}
+		if (hasErrors) {
+			grunt.fail.warn('Errors found during code validation');
 		}
 	});
+
+	grunt.registerTask("fiori-test", ["lint", "check-lint"]);
+	grunt.registerTask("buildapp", ["build"]);
+	grunt.registerTask("default", ["build"]);
 };
